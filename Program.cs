e24ef8c85d58f1488cab11c619aee6ae66b62402
Shane Beyer <54415734+Beyer98@users.noreply.github.com>
2019-11-20 17:11:29 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Serilog;
+using Serilog.Formatting.Compact;
+using System;
+
+namespace CIS174_Project
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                            .Enrich.FromLogContext()
+                            .WriteTo.Console(new CompactJsonFormatter())
+                            .CreateLogger();
+             
+            try
+            {
+                Log.Information("Starting up");
+                CreateWebHostBuilder(args).Build().Run();
+
+            }catch(Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unxpectedly");
+            }finally
+            {
+                Log.CloseAndFlush();
+            }
+           
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                   .UseSerilog()
+                   .UseStartup<Startup>();
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,95 @@
+﻿using CIS174_Project.Data;
+using CIS174_Project.Entities;
+using CIS174_Project.Filters;
+using CIS174_Project.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CIS174_Project
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+ 
+            services.AddDbContext<DataContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDefaultIdentity<UserPerson>(confiq => 
+                {
+                   confiq.User.RequireUniqueEmail = true;
+                }).AddEntityFrameworkStores<DataContext>();
+
+            services.AddAuthentication();
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("IsAdmin", policyBuilder => policyBuilder.AddRequirements(
+                                                 new AdminRequirement(),
+                                                 new IsActiveUser(),
+                                                 new MinimumAgeRequirement(18)));
+                options.AddPolicy("CanEditContent", policyBuilder => policyBuilder.AddRequirements(new ContentEditorRequirement()));
+
+                options.AddPolicy("editor", policyBuilder => policyBuilder.RequireClaim("ContentEditor"));
+            });
+
+            // services.AddScoped<IAuthorizationRequirement, IsActiveUser>();
+            services.AddScoped<IAuthorizationHandler, AdminHandler>();
+            services.AddScoped<IAuthorizationHandler, IsActiveUserHandler>();
+            services.AddScoped<IAuthorizationHandler, MinimumAgeHandler>();
+            services.AddScoped<IAuthorizationHandler, ContentEditorHandler>();
+   
+            //services.AddIdentity<UserPerson, UserRole>(config =>
+            //{  AuthorizationPolicyBuilder
+            //    config.User.RequireUniqueEmail = true;
+
+            //}).AddEntityFrameworkStores<DataContext>(); 
+
+            services.AddScoped<IRecipeService, RecipeService>();
+            services.AddScoped<IAccomplishmentService, AccomplishmentService>();
+            services.AddScoped<IPersonManagerService, PersonManagerService>();
+           
+            services.AddScoped<ValidateModelAttribute>();
+
+            services.AddMvc(config =>
+                    {
+                        config.Filters.Add(typeof(CustomExceptionFilter));
+                    }).SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseAuthentication();
+           
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute("Alternative", 
+                                 "{controller}/{action}/{id?}",
+                              new {controller ="Home", action ="Index", id ="" });
+            });
+        }
+    }
+}
